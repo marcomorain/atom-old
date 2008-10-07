65f3b0a10c2dcfa28e40693c02fadd893dde3b01
marc.omorain <marc.omorain@4ac7a2ae-9456-11dd-b03a-274896f2978a>
2008-10-07 10:47:03 +0000

    added main.cpp

---
+++ trunk/atom/Main.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <Atom.h>
+using namespace std;
+
+int main (int argc, char** argv)
+{
+	parse("(a b (c d) e)");
+
+	while(true)
+	{
+		const int buffer_size = 1024;
+		char buffer[buffer_size];
+		cout << ">" << flush;
+		cin.getline (buffer, buffer_size);
+		if ( strlen ( buffer ) == 0 ) break;
+		parse(buffer);
+	}
+	cout << "Press any key to quit." << endl;
+	char end = cin.get();
+	return EXIT_SUCCESS;
+}
